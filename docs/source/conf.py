74965c6d3ec39c59eb52c5590ad409847f19f8bd
Vamsi Kalagaturu <vamsikalagaturu@gmail.com>
2023-10-11 14:00:16 +0200

    initial rtd docs setup

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'b-it-bots'
+copyright = '2023, www.h-brs.de/en/a2s/b-it-bots'
+author = 'www.h-brs.de/en/a2s/b-it-bots'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = []
---
+++ docs/source/index.rst
@@ -0,0 +1,16 @@
+b-it-bots
+===================================
+
+Welcome to the documentation page of b-it-bots. This is a landing page for all the various projects that we are working on.
+
+Contents
+--------
+
+.. toctree::
+  :maxdepth: 2
+
+* @work
+* @home
+* `KinovaApps <https://b-it-bots.readthedocs.io/projects/kinova_apps/en/latest/>`_
+* Freddy
+* Robile
